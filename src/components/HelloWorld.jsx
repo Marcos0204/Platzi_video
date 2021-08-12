43f6f96cad486e6e2e87e1f584133ada362b1980
Marcos <marcosbastidas301@gmail.com>
2021-08-12 03:50:57 -0400

    proyect_config_finish

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>HelloWorld</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
