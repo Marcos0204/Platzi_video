68fc7b14ad7605b36b76954b474d3e248155d30d
Marcos <marcosbastidas301@gmail.com>
2021-08-15 01:00:26 -0400

    conecting_information_api

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable indent */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
@@ -9,22 +10,36 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
 const App = () => {
+
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+
+  useEffect(() => {
+   fetch('http://localhost:3000/initalState')
+   .then((response) => response.json())
+   .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
     return (
       <div className='App'>
         <Header />
         <Search />
-        <Categories title='Mi lista'>
-          <Carousel>
-            <CarouselItem />
-            <CarouselItem />
-            <CarouselItem />
-          </Carousel>
-        </Categories>
+
+        { videos.mylist > 0 && (
+          <Categories title='Mi lista'>
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+
+        )}
         <Categories title='Tendencias'>
           <Carousel>
             <CarouselItem />
-            <CarouselItem />
-            <CarouselItem />
+            {videos.trends.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
           </Carousel>
         </Categories>
         <Categories title='Originales de platzi video'>
