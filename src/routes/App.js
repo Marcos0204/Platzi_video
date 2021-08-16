71b3a7160edea91ebcbed3223e6200df4f5709c6
Marcos <marcosbastidas301@gmail.com>
2021-08-16 10:33:42 -0400

    login_structure_styles

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../Containers/Home';
+import Login from '../Containers/Login';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
     </BrowserRouter>
   );
 };
