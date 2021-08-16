6e5c664ee4d39146386d302bae187457007343f2
Marcos <marcosbastidas301@gmail.com>
2021-08-16 10:54:17 -0400

    register_structure_styles

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
 /* eslint-disable react/self-closing-comp */
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../Containers/Home';
 import Login from '../Containers/Login';
+import Register from '../Containers/Register';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
     </BrowserRouter>
   );
 };
